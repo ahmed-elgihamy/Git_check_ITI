5ddd1053cd43754f14580c705bc59146313c879b
Mohamed Ramadan <midoramadan100@yahoo.com>
2025-01-06 13:16:41 +0200

    Updated Files

--- HelloITI.cpp
+++ HelloITI.cpp
@@ -8,10 +8,16 @@ public:
     void printHelloITI()
     {
         cout<<"Hello ITI"<<endl;
+        cout<<"Hello ITI 4"<<endl;
     }
 
      void printHelloGIT()
     {
         cout<<"Hello GIT"<<endl;
+    }
+
+     void printHelloGIT()
+    {
+        cout<<"Hello Ramboo"<<endl;
     }
 };
